9d1e7d79fb27f1634c9e4b02f4944bdeda21e466
M. Chowdhury <mintychocolate101@gmail.com>
2018-09-24 19:10:15 -0700

    Update LL.java

--- src/com/frc5104/autopaths/LL.java
+++ src/com/frc5104/autopaths/LL.java
@@ -9,6 +9,7 @@ public class LL extends BreakerPath {
 		add(new DriveTrajectoryWP(new Waypoint[] {
 				new Waypoint(0, 0, 0),
 				new Waypoint(22, 2, 45)
+					//measured at (14, 1, 90)
 			}));
 		add(new DriveStop());
 	}
