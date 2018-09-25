7b5170f17ba395c792d1a4ac94e21525de2d2351
M. Chowdhury <mintychocolate101@gmail.com>
2018-09-24 19:09:10 -0700

    Update CR.java

--- src/com/frc5104/autopaths/CR.java
+++ src/com/frc5104/autopaths/CR.java
@@ -10,6 +10,7 @@ public class CR extends BreakerPath {
 		add(new DriveTrajectoryWP(new Waypoint[] {
 				new Waypoint(0, 0, 0),
 				new Waypoint(15, 4.0 * Constants.AutonomousWP._xAngleMult, 0)
+					//measured at (9, 2.7, 0)
 			}));
 		add(new DriveStop());
 	}
