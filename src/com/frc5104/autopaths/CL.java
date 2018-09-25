6c8d72e60e8ab7e099f85797e8396c107cb30c32
M. Chowdhury <mintychocolate101@gmail.com>
2018-09-24 19:08:50 -0700

    Update CL.java

--- src/com/frc5104/autopaths/CL.java
+++ src/com/frc5104/autopaths/CL.java
@@ -10,6 +10,7 @@ public class CL extends BreakerPath {
 		add(new DriveTrajectoryWP(new Waypoint[] {
 				new Waypoint(0, 0, 0),
 				new Waypoint(12, -5.0 * Constants.AutonomousWP._xAngleMult, 0)
+					//measured at (9, 2.7, 0)
 			}));
 		add(new DriveStop());
 	}
